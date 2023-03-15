930bc3115e7417431c69bd1f2f7ed50e2a4ad0df
Luke <116524469+lkadieguez@users.noreply.github.com>
2023-03-15 14:00:22 -0300

    Add files via upload

--- src/js/titulo-responsivo.js
+++ src/js/titulo-responsivo.js
@@ -1,14 +1,15 @@
 const subtitulos = document.querySelectorAll(".subtitulo");
 
-window.addEventListener("resize", () => {
-  if (window.screen.width <= 415) {
-    subtitulos[0].innerHTML = "EXPERIÊNCIA";
-    subtitulos[1].innerHTML = "FORMAÇÃO";
-  } else {
-    subtitulos[0].innerHTML = "EXPE<br>RIÊN<br>CIA";
-    subtitulos[1].innerHTML = "FORM<br>AÇÃO";
-  }
-});
+// LINHAS USADAS DURANTE O DESENVOLVIMENTO APENAS PARA DEBUG
+// window.addEventListener("resize", () => {
+//   if (window.screen.width <= 415) {
+//     subtitulos[0].innerHTML = "EXPERIÊNCIA";
+//     subtitulos[1].innerHTML = "FORMAÇÃO";
+//   } else {
+//     subtitulos[0].innerHTML = "EXPE<br>RIÊN<br>CIA";
+//     subtitulos[1].innerHTML = "FORM<br>AÇÃO";
+//   }
+// });
 
 if (window.screen.width <= 415) {
   subtitulos[0].innerHTML = "EXPERIÊNCIA";
